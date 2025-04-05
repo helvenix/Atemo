8914a5034b8d83b349400e4c1214f6664f5e8402
Helvennn <enixgregory@gmail.com>
2025-04-05 12:42:34 +0700

    User Model Added

---
+++ Server/models/User.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    id: { type: Number, required: true, unique: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profilePicture: { type: String },
+    preferences: { type: mongoose.Schema.Types.Mixed }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', UserSchema);
