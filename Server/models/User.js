7d4350bdb0d77281b6c1c388ee51b32737840783
Helvennn <enixgregory@gmail.com>
2025-04-06 08:49:01 +0700

    UID Added to user model

--- Server/models/User.js
+++ Server/models/User.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
+    uid: { type: Number, required: true, unique: true },
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
