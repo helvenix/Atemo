f795f303e8eb8096f0a1d55186e535ad5601ce2f
Helvennn <enixgregory@gmail.com>
2025-04-10 14:19:22 +0700

    Maximum Letters for Task's Title Adjusted

--- Client/app/(main)/add/page.tsx
+++ Client/app/(main)/add/page.tsx
@@ -32,7 +32,7 @@ const taskSchema = z.object({
     title: z
         .string()
         .min(1, {message: "title required"})
-        .max(24, {message: "Maximum 24 characters"}),
+        .max(20, {message: "Maximum 20 characters"}),
     notes: z
         .string(),
     start: z
