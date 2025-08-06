e7c4d269255df85a23ecb647725e5fe26de87ae8
Helvennn <enixgregory@gmail.com>
2025-08-06 12:20:32 +0700

    App list ui and utils components added

---
+++ components/layout/list/type.ts
@@ -0,0 +1,51 @@
+export interface Task {
+    _id: string;
+    userId: string;
+    title: string;
+    notes?: string;
+    start: string;
+    deadline: string;
+    completedStatus?: boolean
+}
+
+export interface Event {
+    _id: string;
+    userID: string;
+    title: string;
+    notes?: string;
+    start: string;
+    end?: string;
+    recurrenceRule: string;
+    dismissed?: boolean;
+}
+
+export interface CarouselMeta {
+    remaining: number;
+    ratio: number;
+    urgent: boolean;
+    timeString: string;
+}
+
+export interface CarouselProps<T> {
+    items: T[];
+    now: Date;
+    hovered: boolean;
+    focus: number;
+    setHovered: React.Dispatch<React.SetStateAction<boolean>>;
+    setFocus: (idx: number) => void;
+}
+
+export interface FocusCardProps<T>{
+    item: T;
+    timeRemaining: string;
+    timeRatio: number;
+    urgent: boolean;
+    setHovered: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export interface MinimalCardProps<T>{
+    item: T;
+    timeRemaining: string;
+    urgent: boolean;
+    conceal: boolean;
+}
